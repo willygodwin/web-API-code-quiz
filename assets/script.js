3de7382c6afb3c07eb6f08c3b2600ed6c9177350
willy <willygodwin47@gmail.com>
2020-12-01 19:19:18 +0800

    Added JS document

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+var questions = {
+    1: "Which of the following Attribute is used to include External JS code inside your HTML Document",
+    2: "Which of the following is not considered as an error in JavaScript?",
+    3: "The statement a===b refers to"
+};
+
+var answers = {
+    1: ["src", "ext", "script", "link"],
+    2: ["Syntax error", "Missing semi-colons", "Division By zero", "All of the above"],
+    3: ["Both a and b are equal in value, type and reference address", "Both a and b are equal in value", "Both a and b are equal in value and type", "There is no such statement"]
+};
+
+var answer = {
+    1: "src",  
+    2: "Division By zero", 
+    3: "Both a and b are equal in value and type"
+
+};
+
+
